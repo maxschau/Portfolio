182e40ec23451d82dc18315ad48085e73093f841
Max Torre Schau <maxts@Maxs-MacBook-Pro.local>
2020-08-13 23:41:40 +0200

    Changed margin on about me

--- frontend/src/Views/FrontPageView.jsx
+++ frontend/src/Views/FrontPageView.jsx
@@ -115,7 +115,7 @@ export const FrontPageView = props => {
                             </TabPanel>
                             <TabPanel value={value} index={1}>
                                 <Box>
-                                    <Box textAlign="left" padding="8px">
+                                    <Box textAlign="left" paddingLeft="8px" paddingRight="8px" mt="16px">
                                         <Typography variant="body1">
                                             I'm a 22 year old man from Oslo with a great passion about coding. I have always been interested in computers, but started with development/coding in 2015. And I have not been looking back since.
                                             Currenly I'm spending my time as a full-time-student at NTNU, while working as a teaching assistant in the subjects Databases and Introductory Programming for Teachers.
